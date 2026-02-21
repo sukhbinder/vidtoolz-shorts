90ed82a2ed051da323716494fbf024063c1ac7cc
Sukhbinder Singh <sukhbindersingh@gmail.com>
2026-02-21 12:05:16 +0000

    Fix typo in  test case. and realcase

--- tests/test_vidtoolz_shorts.py
+++ tests/test_vidtoolz_shorts.py
@@ -1,7 +1,10 @@
-import pytest
-import vidtoolz_shorts as w
+import os
+from argparse import ArgumentParser
+from pathlib import Path
 
-from argparse import Namespace, ArgumentParser
+import pytest
+
+import vidtoolz_shorts as w
 
 
 def test_create_parser():
@@ -23,3 +26,21 @@ def test_plugin(capsys):
     w.shorts_plugin.hello(None)
     captured = capsys.readouterr()
     assert "Hello! This is an example ``vidtoolz`` plugin." in captured.out
+
+
+IN_GITHUB_ACTIONS = os.getenv("GITHUB_ACTIONS") == "true"
+
+
+@pytest.mark.skipif(IN_GITHUB_ACTIONS, reason="Test doesn't work in Github Actions.")
+def test_realcase_shorts(tmpdir):
+    outfile = tmpdir / "test_intro.mp4"
+    testdata = Path(__file__).parent / "test_data"
+    introfile = testdata / "test.mp4"
+    subparser = ArgumentParser().add_subparsers()
+    parser = w.create_parser(subparser)
+
+    argv = [str(introfile), "-o", str(outfile), "-i", "Hello", "-i", "World"]
+    args = parser.parse_args(argv)
+    args.func = None
+    w.shorts_plugin.run(args)
+    assert outfile.exists()
